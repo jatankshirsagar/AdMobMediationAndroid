eb09c628056520b6e4373385d8da8ce117048f52
JatanPC <jatan.kshirsagar@gmail.com>
2017-10-22 13:29:11 +0530

    First Commit

--- app/src/main/java/com/jpk/myapplication/Utils.kt
+++ app/src/main/java/com/jpk/myapplication/Utils.kt
@@ -1,19 +1,25 @@
 package com.jpk.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.google.ads.mediation.facebook.FacebookAdapter
 import com.google.ads.mediation.inmobi.InMobiAdapter
 import com.google.ads.mediation.inmobi.InMobiNetworkKeys
 import com.google.ads.mediation.inmobi.InMobiNetworkValues
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
 import com.mopub.mobileads.dfp.adapters.MoPubAdapter
+import com.vungle.mediation.VungleExtrasBuilder
+import com.vungle.mediation.VungleInterstitialAdapter
 
 
 object Utils {
 
-    fun loadAds(view: View) {
+    fun loadBannerAd(view: View) {
         // Sample adapter banner.
         val mAdapterAdView: AdView = view as AdView
         val mopubExtras = MoPubAdapter.BundleBuilder()
@@ -26,11 +32,63 @@ object Utils {
         inMobiExtras.putString(InMobiNetworkKeys.AGE_GROUP,
                 InMobiNetworkValues.BETWEEN_35_AND_54)
 
+        // build network extras bundle
+        val vungleExtras = VungleExtrasBuilder()
+                .setUserId("userId")
+                .setSoundEnabled(false)
+                .build()
+
         val bannerAdRequest = AdRequest.Builder()
                 .addNetworkExtrasBundle(FacebookAdapter::class.java, facebookExtras)
                 .addNetworkExtrasBundle(MoPubAdapter::class.java, mopubExtras)
                 .addNetworkExtrasBundle(InMobiAdapter::class.java, inMobiExtras)
+                .addNetworkExtrasBundle(VungleInterstitialAdapter::class.java, vungleExtras)
                 .build()
+
         mAdapterAdView.loadAd(bannerAdRequest)
+    }
+
+    fun loadInterstitialAd(context: Context) {
+        val mInterstitialAd = InterstitialAd(context)
+        mInterstitialAd.adUnitId = context.getString(R.string.admob_id)
+
+        val vungleExtras = VungleExtrasBuilder()
+                .setUserId("userId")
+                .setSoundEnabled(false)
+                .build()
+
+        val adRequest = AdRequest.Builder()
+                .addNetworkExtrasBundle(VungleInterstitialAdapter::class.java, vungleExtras)
+                .addTestDevice("BD2B6EA9B18672FF166D0E9CB70B9C26")
+                .build()
+        mInterstitialAd.loadAd(adRequest)
+
+        mInterstitialAd.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.i("Ads", "onAdLoaded")
+                mInterstitialAd.show()
+            }
+
+            override fun onAdFailedToLoad(errorCode: Int) {
+                // Code to be executed when an ad request fails.
+                Log.i("Ads", "onAdFailedToLoad")
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when the ad is displayed.
+                Log.i("Ads", "onAdOpened")
+            }
+
+            override fun onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Log.i("Ads", "onAdLeftApplication")
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when when the interstitial ad is closed.
+                Log.i("Ads", "onAdClosed")
+            }
+        }
     }
 }
--- app/src/main/java/com/jpk/myapplication/activites/MainActivity.kt
+++ app/src/main/java/com/jpk/myapplication/activites/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jpk.myapplication.activites
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.AppCompatButton
+import android.view.View
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.jpk.myapplication.R
@@ -15,32 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         MobileAds.initialize(this, getString(R.string.admob_id))
         val mAdapterAdView: AdView = findViewById(R.id.adView)
-        Utils.loadAds(mAdapterAdView)
+        Utils.loadBannerAd(mAdapterAdView)
+
+        val btnLoadAd: AppCompatButton = findViewById(R.id.btnLoadAd)
+        btnLoadAd.setOnClickListener(View.OnClickListener { Utils.loadBannerAd(mAdapterAdView) })
+
+        val btnLoadInterstitialAd: AppCompatButton = findViewById(R.id.btnLoadInterstitialAd)
+        btnLoadInterstitialAd.setOnClickListener(View.OnClickListener { Utils.loadInterstitialAd(this) })
     }
 
-    /*fun loadAd() {
-        // Sample adapter banner.
-        val mAdapterAdView: AdView = findViewById(R.id.adView)
-        val mopubExtras = MoPubAdapter.BundleBuilder()
-                .build()
-        val facebookExtras = FacebookAdapter.FacebookExtrasBundleBuilder()
-                .setNativeAdChoicesIconExpandable(false)
-                .build()
-
-        val inMobiExtras = Bundle()
-        inMobiExtras.putString(InMobiNetworkKeys.AGE_GROUP,
-                InMobiNetworkValues.BETWEEN_35_AND_54)
-
-        val bannerAdRequest = AdRequest.Builder()
-                .addNetworkExtrasBundle(FacebookAdapter::class.java, facebookExtras)
-                .addNetworkExtrasBundle(MoPubAdapter::class.java, mopubExtras)
-                .addNetworkExtrasBundle(InMobiAdapter::class.java, inMobiExtras)
-                .build()
-        mAdapterAdView.loadAd(bannerAdRequest)
-    }*/
-
-    override fun onPause() {
-        super.onPause()
-        System.exit(1)
-    }
 }
